3b42767ffdca85cb4216edb1d8ec750fe3746c72
thetngonmay <thetngon.dev@gmail.com>
2025-08-10 13:28:00 +0700

    Added Popup Menu in Notification Page

--- lib/ui/notification_page.dart
+++ lib/ui/notification_page.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:youtube_clone/ui/search_page.dart';
+import 'package:youtube_clone/ui/setting_page.dart';
 import 'package:youtube_clone/ui/widgets/notification_card.dart';
 
 class NotificationPage extends StatefulWidget {
@@ -34,7 +35,34 @@ class NotificationPageState extends State<NotificationPage>{
           actions: [
             IconButton(onPressed: onCastClick, icon: Icon(Icons.cast)),
             IconButton(onPressed: onSearchbar, icon: Icon(Icons.search)),
-            IconButton(onPressed: onMore, icon: Icon(Icons.more_vert)),
+            PopupMenuButton<String>(
+              onSelected: (String result){
+                if(result=='setting'){
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context)=>SettingPage())
+                  );
+                }
+              },
+              itemBuilder: (BuildContext context)=> <PopupMenuEntry<String>>[
+                const PopupMenuItem<String>(
+                  value: 'setting',
+                  child: Text('Setting'),
+                ),
+                const PopupMenuItem<String>(
+                  value: 'tv',
+                  child: Text('Watch on TV'),
+                ),
+                const PopupMenuItem<String>(
+                  value: 'tc',
+                  child: Text('Terms and privacy policy'),
+                ),
+                const PopupMenuItem<String>(
+                  value: 'help',
+                  child: Text('Help and feedback'),
+                )
+              ],
+            )
 
           ],
         ),
